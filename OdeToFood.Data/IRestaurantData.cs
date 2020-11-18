e6f10001f7c934eebcc92efb34a849203ba54d90
Dom <dship13@wgu.edu>
2020-11-17 23:01:03 -0700

    Added bootstrap 3, Then added models and model bindings

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -6,7 +6,8 @@ namespace OdeToFood.Data
 {
     public interface IRestaurantData
     {
-        IEnumerable<Restaurant> GetAll();
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
 
         public class InMemoryRestaurantData : IRestaurantData
         {
@@ -22,11 +23,17 @@ namespace OdeToFood.Data
                 };
             }
 
-            public IEnumerable<Restaurant> GetAll()
+            public Restaurant GetById(int id)
+            {
+                return restaurants.SingleOrDefault(r => r.Id == id);
+            }
+
+            public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
             {
                 return from r in restaurants
-                    orderby r.Name
-                    select r;
+                       where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                       orderby r.Name
+                       select r;
             }
         }
     }
--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -16,6 +16,9 @@ namespace OdeToFood.Pages.Restaurants
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+
         private readonly IConfiguration config;
         private readonly IRestaurantData restaurantData;
 
@@ -28,7 +31,7 @@ namespace OdeToFood.Pages.Restaurants
         public void OnGet()
         {
             Message = config["Message"];
-            Restaurants = restaurantData.GetAll();
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
         }
     }
 }
